6ee0112f890e0334b0671d45910cc369a4436694
NurkhanKenesov <nurkhan200505@gmail.com>
2025-02-23 03:21:38 +0500

    SwiftUI

---
+++ Custom Fonts/Custom Fonts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom Fonts
+//
+//  Created by Nurkhan Kenesov on 23.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+            .font(.custom("OpenSans", size: 34))
+            .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Custom Fonts/Custom Fonts/Custom_FontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_FontsApp.swift
+//  Custom Fonts
+//
+//  Created by Nurkhan Kenesov on 23.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Custom_FontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
