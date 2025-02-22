2cd572a61c11449434d0b0266fa856baac2cfa29
NurkhanKenesov <nurkhan200505@gmail.com>
2025-02-23 00:38:03 +0500

    swiftUI

---
+++ SafeArea/SafeArea/SafeAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeAreaApp.swift
+//  SafeArea
+//
+//  Created by Nurkhan Kenesov on 21.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SafeAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
