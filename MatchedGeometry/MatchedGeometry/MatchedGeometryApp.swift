3ee2c9034a3873b85f2049324fc2731c75e0b950
NurkhanKenesov <nurkhan200505@gmail.com>
2025-02-25 05:13:33 +0500

    Matched Geometry

---
+++ MatchedGeometry/MatchedGeometry/MatchedGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryApp.swift
+//  MatchedGeometry
+//
+//  Created by Nurkhan Kenesov on 24.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
